1523e07b1fdd98f7277c2133c9f8af67f457d601
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-09-01 16:31:15 +0100

    making css scoped

--- resources/js/components/Container.jsx
+++ resources/js/components/Container.jsx
@@ -1,15 +1,8 @@
+import styles from '../../css/modules/components/Container.module.css';
+
 export default function Container({style, children}) {
     return <>
-        <style>
-        {`
-          /* Scoped styles */
-          .container {
-            max-width: 280px;
-            margin: auto;
-          }
-        `}
-        </style>
-        <div className="container" style={{...style}}>
+        <div className={styles.container} style={{...style}}>
             {children}
         </div>
     </>;
