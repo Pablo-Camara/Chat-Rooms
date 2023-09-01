b03c1bac52f1a5b7365281e442a783cb02643218
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-09-01 21:48:10 +0100

    will use username, and first/last name

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'firstName',
+        'lastName',
+        'username',
         'password',
     ];
 
