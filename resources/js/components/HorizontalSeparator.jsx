0d7e01b0a65fba52679649cee8d9597e95a12584
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-09-01 16:42:01 +0100

    making css scoped

--- resources/js/components/HorizontalSeparator.jsx
+++ resources/js/components/HorizontalSeparator.jsx
@@ -1,16 +1,9 @@
+
+import styles from '../../css/modules/components/HorizontalSeparator.module.css';
+
 export default function HorizontalSeparator({style, children}) {
     return <>
-        <style>
-        {`
-          /* Scoped styles */
-          .hseparator {
-            background: #D8D8D8;
-            border: 1px solid #979797;
-            height: 4px;
-          }
-        `}
-        </style>
-        <div className="hseparator" style={{...style}}>
+        <div className={styles.hseparator} style={{...style}}>
             {children}
         </div>
     </>
