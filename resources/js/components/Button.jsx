c3b716ca57a55afba5f00feee0c3270fe1e041c8
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-09-01 16:27:39 +0100

    making css scoped

--- resources/js/components/Button.jsx
+++ resources/js/components/Button.jsx
@@ -1,23 +1,8 @@
+import styles from '../../css/modules/components/Button.module.css';
+
 export default function Button({style, onClick, children}) {
     return <>
-        <style>
-        {`
-          /* Scoped styles */
-          .button {
-            background: #013150;
-            border: 1px solid #979797;
-            border-radius: 8px;
-            font-family: Verdana;
-            font-size: 18px;
-            color: #FFFFFF;
-            text-align: center;
-            height: 35px;
-            line-height: 35px;
-            cursor: pointer;
-          }
-        `}
-        </style>
-        <div className="button"
+        <div className={styles.button}
             style={{...style}}
             onClick={onClick}>
             {children}
