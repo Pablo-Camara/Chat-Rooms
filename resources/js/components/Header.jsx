ed12258746e0d3b699f87ed1b1c31e5d8ef96b54
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-09-01 16:39:34 +0100

    making css scoped

--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -1,50 +1,24 @@
+import styles from '../../css/modules/components/Header.module.css';
+
 export default function Header({type, style, children}) {
     return <>
-        <style>
-            {`
-                /* Scoped styles */
-                h1, h2 {
-                    font-weight: normal;
-                }
-            `}
-        </style>
-
         {
             type === 'h1'
             &&
             <>
-                <style>
-                    {`
-                        /* Scoped styles */
-                        h1 {
-                            font-family: Verdana;
-                            font-size: 20px;
-                            color: #000000;
-                            text-align: center;
-                        }
-                    `}
-                </style>
-                <h1 style={{...style}}>{children}</h1>
+                <h1 className={styles.defaultHeader} style={{...style}}>
+                    {children}
+                </h1>
             </>
         }
-
 
         {
             type === 'h2'
             &&
             <>
-                <style>
-                    {`
-                        /* Scoped styles */
-                        h2 {
-                            font-family: Verdana;
-                            font-size: 20px;
-                            color: #000000;
-                            text-align: center;
-                        }
-                    `}
-                </style>
-                <h2 style={{...style}}>{children}</h2>
+                <h2 className={styles.defaultHeader} style={{...style}}>
+                    {children}
+                </h2>
             </>
         }
     </>
