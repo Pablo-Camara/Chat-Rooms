4ba5f420b17ff3de5a823021c3335439c8d9cf28
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-09-01 16:49:48 +0100

    making css scoped

--- resources/js/components/TextBox.jsx
+++ resources/js/components/TextBox.jsx
@@ -1,18 +1,7 @@
+import styles from '../../css/modules/components/TextBox.module.css';
 export default function TextBox({children}) {
     return <>
-        <style>
-        {`
-          /* Scoped styles */
-          .text-box-container {
-            background: #D8D8D8;
-            border: 1px solid #979797;
-            width: 280px;
-            height: 31px;
-            margin-top: 6px;
-          }
-        `}
-        </style>
-        <div className="text-box-container">
+        <div className={styles.textBoxContainer}>
             {/* <input type="text" /> */}
         </div>
     </>;
