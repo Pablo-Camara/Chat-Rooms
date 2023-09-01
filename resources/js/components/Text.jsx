1f7bf9fd5c19f2b80eba3cc36afc1a77ff7508ce
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-09-01 16:46:55 +0100

    making styles scoped

--- resources/js/components/Text.jsx
+++ resources/js/components/Text.jsx
@@ -1,17 +1,7 @@
+import styles from '../../css/modules/components/Text.module.css';
 export default function Text({style, onClick, children}) {
     return <>
-        <style>
-        {`
-          /* Scoped styles */
-          .text {
-            font-family: Verdana;
-            font-size: 16px;
-            color: #000000;
-            text-align: center;
-          }
-        `}
-        </style>
-        <div className="text"
+        <div className={styles.text}
             style={{...style}}
             onClick={onClick}>
             {children}
