cd8892a142f4c07c1be66a62b8f52afb43319e3c
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-09-03 20:06:22 +0100

    tsrated using ChatRoomService methods

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\ChatRoom;
 use App\Models\User;
+use App\Services\ChatRoomService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class ChatController extends Controller
 {
@@ -15,66 +13,17 @@ class ChatController extends Controller
         $user = $request->user();
         $destinationUser = User::find($userId);
 
-        $chatRoom = ChatRoom::where(function($query) use ($user, $destinationUser) {
-            return $query->where(
-                function($query) use ($user, $destinationUser) {
-                    return $query->where('sender_id','=', $user->id)
-                        ->where('receiver_id', '=', $destinationUser->id);
-                }
-            )->orWhere(
-                function($query) use ($user, $destinationUser) {
-                    return $query->where('sender_id','=', $destinationUser->id)
-                        ->where('receiver_id', '=', $user->id);
-                }
-            );
-        })->where('is_private', '=', 1)->first();
-
-        if (empty($chatRoom)) {
-            $chatRoom = new ChatRoom();
-            $chatRoom->title = 'Private Chat';
-            $chatRoom->max_users = 2;
-            $chatRoom->is_private = 1;
-            $chatRoom->sender_id = $user->id;
-            $chatRoom->receiver_id = $destinationUser->id;
-            $chatRoom->password = Hash::make(Str::random(255));
-            $chatRoom->save();
-        }
+        $chatRoom = ChatRoomService::getOrCreatePrivateRoomBetweenUsers($user, $destinationUser);
 
         $chatRoomMessages = $chatRoom->messages->all();
 
-        return response()->json([
-            'chatRoom' => [
-                'id' => $chatRoom->id,
-                'messages' => array_map(
-                    function ($chatMessage) {
-                        return [
-                            'message' => $chatMessage->message,
-                            'dateSent' => $chatMessage->created_at,
-                            'sender' => [
-                                'id' => $chatMessage->sender->id,
-                                'username' => $chatMessage->sender->username
-                            ],
-                            'receiver' => [
-                                'id' => $chatMessage->receiver->id,
-                                'username' => $chatMessage->receiver->username
-                            ],
-                        ];
-                    },
-                    $chatRoomMessages
-                )
-            ],
-            'user' => [
-                'id' => $user->id,
-                'firstName' => $user->firstName,
-                'lastName' => $user->lastName,
-                'username' => $user->username,
-            ],
-            'destinationUser' => [
-                'id' => $user->id,
-                'firstName' => $destinationUser->firstName,
-                'lastName' => $destinationUser->lastName,
-                'username' => $destinationUser->username,
-            ]
-        ]);
+        return response()->json(
+            ChatRoomService::getPrivateChatRoomResponseData(
+                $chatRoom,
+                $chatRoomMessages,
+                $user,
+                $destinationUser
+            )
+        );
     }
 }
