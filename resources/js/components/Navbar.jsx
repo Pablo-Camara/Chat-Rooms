b7a7277aaeb7ef51bd4915f44dd2478e19093cbd
Pablo Câmara <pablo.camara.pt@gmail.com>
2023-09-08 12:17:53 +0100

    scoped css for the notification bar/container

--- resources/js/components/Navbar.jsx
+++ resources/js/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import styles from '../../css/modules/components/Navbar.module.css';
+import navbarStyles from '../../css/modules/components/Navbar.module.css';
+import notificationsStyles from '../../css/modules/components/Notifications.module.css';
 import Container from './Container';
 import { useNavigate } from "react-router";
 
 export default function Navbar({ authenticated }) {
-    let finalClassName = authenticated ? styles.authenticatedNavbar : styles.unauthenticatedNavbar;
+    let finalClassName = authenticated ? navbarStyles.authenticatedNavbar : navbarStyles.unauthenticatedNavbar;
     const navigate = useNavigate();
     return <>
         <div className={finalClassName}>
@@ -37,42 +38,15 @@ export default function Navbar({ authenticated }) {
                 </Container>
             }
         </div>
-        <Container style={{
-            background: '#123c58',
-            paddingBottom: '4px'
-        }}>
-            <div style={{
-                fontSize: '12px',
-                padding: '12px',
-                textAlign: 'right',
-                color: 'white'
-            }}>Notifications <span style={{
-                color: 'red',
-                marginLeft: '6px',
-                fontSize: '14px'
-            }}>X</span></div>
+        <Container className={notificationsStyles.notificationsContainer}>
+            <div className={notificationsStyles.notificationsHeader}>
+                Notifications <span>X</span></div>
 
-            <div style={{
-                width: '280px',
-                margin: 'auto',
-                background: 'white',
-                fontSize: '12px',
-                padding: '8px',
-                textAlign: 'right',
-                marginBottom: '12px'
-            }}>
+            <div className={notificationsStyles.notificationItem}>
                 You have 2 new messages from <b>@johndoe</b>
             </div>
 
-            <div style={{
-                width: '280px',
-                margin: 'auto',
-                background: 'white',
-                fontSize: '12px',
-                padding: '8px',
-                textAlign: 'right',
-                marginBottom: '12px'
-            }}>
+            <div className={notificationsStyles.notificationItem}>
                 New friend request from <b>@johndoe</b>
             </div>
         </Container>
